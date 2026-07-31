af61ddf547dc49aeb8ce177bcf21ea5c0aeeaab2
Rinzller <jrseriel@aim.com>
2026-07-30 20:15:20 -0400

    Polish app icon and title accent

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,6 @@
 fn main() {
+    println!("cargo:rerun-if-changed=../public/icons/favicon.ico");
+    println!("cargo:rerun-if-changed=../public/icons/favicon.png");
+    println!("cargo:rerun-if-changed=../public/icons/favicon.svg");
     tauri_build::build();
 }
